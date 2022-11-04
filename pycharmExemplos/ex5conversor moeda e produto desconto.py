4cc768a0d82593c27eadf79328b8a5cac7d5c74d
beatrizornelas <101950613+beatrizornelas@users.noreply.github.com>
2022-11-03 23:24:43 -0300

    Delete pycharmExemplos directory

--- pycharmExemplos/ex5conversor moeda e produto desconto.py
+++
@@ -1,9 +0,0 @@
-#conversor de moeda
-real = float(input('Quanto dinheiro você tem na carteira? R$'))
-dolar = real / 4.65
-print('Com R${} você pode comprar US${:.2f}'.format(real, dolar))
-print('-' *12)
-##produto com desconto
-preço = float(input('Qual é o preço do produto? R$'))
-novo = preço - (preço * 5 / 100)
-print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}'.format(preço, novo))
